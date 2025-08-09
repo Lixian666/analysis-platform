4eaae0b2ab4f386f5e9f1d3ac983f111350d5a49
lixian <lixian@21at.com.cn>
2025-08-09 11:43:39 +0800

    feat(experiment): 添加真趣轨迹点类并更新相关功能
    - 新增 LocationPoint2 类用于表示真趣轨迹点
    - 修改 RyTask 类中的方法，使用新的轨迹点类
    - 更新 ZQOpenApi 类中的 getListOfPoints 方法，支持自定义时间范围
    - 添加将轨迹数据保存为 JSON 文件的功能

---
+++ modules/src/main/java/com/jwzt/modules/experiment/domain/LocationPoint2.java
@@ -0,0 +1,15 @@
+package com.jwzt.modules.experiment.domain;
+
+import lombok.Data;
+
+/**
+ * @description: 真趣轨迹点
+ * @author: li
+ * @date: 2025/08/06 16:05
+ */
+@Data
+public class LocationPoint2 {
+    private Double longitude;
+    private Double latitude;
+    private String time;
+}
