1bd034b6818ecb552ceb79ac139aa6aafbf2fba7
“Li” <735143929@qq.com>
2025-09-16 10:20:18 +0800

    feat(experiment): 添加信标信息管理功能
    - 新增信标信息管理的 API 接口和前端页面
    - 实现信标信息的查询、新增、修改、删除和导出功能
    -优化页面布局和样式，增加搜索和筛选功能

---
+++ ruoyi-vue3/src/api/experiment/beaconInfo.js
@@ -0,0 +1,44 @@
+import request from '@/utils/request'
+
+// 查询信标信息列表
+export function listBeaconInfo(query) {
+  return request({
+    url: '/experiment/beaconInfo/list',
+    method: 'get',
+    params: query
+  })
+}
+
+// 查询信标信息详细
+export function getBeaconInfo(id) {
+  return request({
+    url: '/experiment/beaconInfo/' + id,
+    method: 'get'
+  })
+}
+
+// 新增信标信息
+export function addBeaconInfo(data) {
+  return request({
+    url: '/experiment/beaconInfo',
+    method: 'post',
+    data: data
+  })
+}
+
+// 修改信标信息
+export function updateBeaconInfo(data) {
+  return request({
+    url: '/experiment/beaconInfo',
+    method: 'put',
+    data: data
+  })
+}
+
+// 删除信标信息
+export function delBeaconInfo(id) {
+  return request({
+    url: '/experiment/beaconInfo/' + id,
+    method: 'delete'
+  })
+}
