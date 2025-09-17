927e49e50198d389046113e01acb19d6b7b3b9c9
“Li” <735143929@qq.com>
2025-09-17 17:03:49 +0800

    refactor(experiment): 重构 ZQOpenApi 类并优化 token 获取逻辑提升获取效率
    - 使用 Redis 缓存 token 和 signId，减少接口调用次数
    - 优化 token 获取和刷新逻辑，提高系统稳定性- 删除未使用的代码和注释，提高代码可读性
    - 重构部分方法，提高代码复用性和维护性

--- modules/src/main/java/com/jwzt/modules/experiment/utils/third/zq/ZQOpenApi.java
+++ modules/src/main/java/com/jwzt/modules/experiment/utils/third/zq/ZQOpenApi.java
@@ -15,6 +15,7 @@ import com.jwzt.modules.experiment.utils.http.HttpUtils;
 import com.jwzt.modules.experiment.utils.third.zq.domain.SubReceiveData;
 import com.jwzt.modules.experiment.utils.third.zq.domain.SubscribeResult;
 import com.jwzt.modules.experiment.utils.third.zq.domain.TagScanUwbData;
+import com.ruoyi.common.core.redis.RedisCache;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,36 +27,30 @@ import java.net.SocketTimeoutException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
+import java.util.*;
+import java.util.concurrent.TimeUnit;
 
 /**
  * 真趣定位api
  */
 @Service
 public class ZQOpenApi {
+
     @Autowired
     private BaseConfig baseConfig;
 
+    @Autowired
+    private RedisCache redisCache;
+
     private static final Logger log = LoggerFactory.getLogger(HttpUtils.class);
+
+    private static final String TOKEN_KEY = "ZQ:TOKEN";   // token缓存key
+    private static final String SIGNID_KEY = "ZQ:SIGNID"; // signId缓存key
 
     private static String licence = null;
 
-    private static String accessToken = null;
-
-    private static String signId = null;
-
-    private static String expireAt = null;
-
-    private static JoySuchResponse<TokenEntity> response = null;
-
     /**
      * 获取UWB信标列表
-     *
-     * @return UWB信标列表的JSON字符串结果
      */
     public String getListOfUWBBeacons() {
         Map<String, String> headers = getHeaders();
@@ -63,8 +58,7 @@ public class ZQOpenApi {
         String jsonBody = JSONObject.toJSONString(new HashMap<String, Object>() {{
             put("pageSize", 1000);
         }});
-        String result = sendPost(baseConfig.getJoysuch().getApi().getBeacons(), headers, jsonBody);
-        return result;
+        return sendPost(baseConfig.getJoysuch().getApi().getBeacons(), headers, jsonBody);
     }
 
     public String getTagStateHistoryOfTagID(String buildingID, String tagID, String startTime, String endTime){
@@ -96,24 +90,6 @@ public class ZQOpenApi {
 
     /**
      * 发送订阅请求
-     *
-     * 订阅类型
-     * location：位置结果
-     * zeroDimEnterLeave：终端进出圈事件
-     * rail：围栏事件
-     * area：区域事件
-     * oneKeyAlarm：一键报警消息
-     * bltOnOffLine：终端定位在离线消息
-     * lowpower：终端低电量
-     * textCmdData：终端下行结果
-     * bltOnOffLineV2：终端通信离线消息
-     * bltProtocolData：标签上报信息
-     * dataReportingData：终端特有数据消息
-     * cmdResult：终端下行cmd结果
-     *
-     * @param type 订阅类型
-     * @param data 订阅数据
-     * @return 订阅结果,
      */
     public SubscribeResult httpSubscriber(String type, SubReceiveData data){
         Map<String, String> headers = getHeaders();
@@ -128,23 +104,6 @@ public class ZQOpenApi {
 
     /**
      * 取消订阅请求
-     *
-     * 订阅类型
-     * location：位置结果
-     * zeroDimEnterLeave：终端进出圈事件
-     * rail：围栏事件
-     * area：区域事件
-     * oneKeyAlarm：一键报警消息
-     * bltOnOffLine：终端定位在离线消息
-     * lowpower：终端低电量
-     * textCmdData：终端下行结果
-     * bltOnOffLineV2：终端通信离线消息
-     * bltProtocolData：标签上报信息
-     * dataReportingData：终端特有数据消息
-     * cmdResult：终端下行cmd结果
-     *
-     * @param subscribeId 订阅数据
-     * @return 取消订阅结果
      */
     public SubscribeResult httpUnSubscriber(String subscribeId){
         Map<String, String> headers = getHeaders();
@@ -157,13 +116,7 @@ public class ZQOpenApi {
     }
 
     /**
-     * 获取指定卡片在指定时间段内的定位点列表
-     *
-     * @param cardId 卡片ID
-     * @param buildId 建筑ID
-     * @param startTime 开始时间，格式为"yyyy-MM-dd HH:mm:ss"
-     * @param endTime 结束时间，格式为"yyyy-MM-dd HH:mm:ss"
-     * @return 定位点列表的JSON字符串结果
+     * 获取定位点
      */
     public String getListOfPoints(String cardId, String buildId, String startTime, String endTime) {
         Map<String, String> headers = getHeaders();
@@ -174,8 +127,7 @@ public class ZQOpenApi {
             put("endTime", DateTimeUtils.convertToTimestamp(endTime));
             put("locationType", "real");
         }});
-        String buildResult = sendPost(baseConfig.getJoysuch().getApi().getPoints(), headers, jsonBody);
-        return buildResult;
+        return sendPost(baseConfig.getJoysuch().getApi().getPoints(), headers, jsonBody);
     }
 
     public JSONObject getListOfCards() {
@@ -188,9 +140,7 @@ public class ZQOpenApi {
             String firstItemStr = dataArray.getString(0);
             JSONObject firstItemJson = JSONObject.parseObject(firstItemStr);
             buildId = firstItemJson.getString("uuid");
-            // 使用 buildId
         } else {
-            // 处理 data 为空的情况
             throw new RuntimeException("data 数组为空，无法提取 buildId");
         }
 
@@ -205,31 +155,35 @@ public class ZQOpenApi {
 
         String result = sendPost(baseConfig.getJoysuch().getApi().getCards(), headers, jsonBody);
         return JSONObject.parseObject(result);
-    };
+    }
 
+    /**
+     * 核心：构建请求头，使用 Redis 缓存 token
+     */
     public Map<String, String> getHeaders() {
         long timestamp = System.currentTimeMillis();
-        if (response == null || accessToken == null || signId == null || expireAt == null){
-            response = getAccessToken(baseConfig.getJoysuch().getUsername(), baseConfig.getJoysuch().getPassword());
-            accessToken = response.getData().getAccessToken();
-            signId = response.getData().getSignId();
-            expireAt = String.valueOf(response.getData().getExpireAt());
-        }
-        if (timestamp - Long.parseLong(expireAt) > 0){
-            response = refreshAccessToken();
-            accessToken = response.getData().getAccessToken();
-            signId = response.getData().getSignId();
-            expireAt = String.valueOf(response.getData().getExpireAt());
 
+        // 从Redis取
+        String accessToken = redisCache.getCacheObject(TOKEN_KEY);
+        String signId = redisCache.getCacheObject(SIGNID_KEY);
+
+        // 如果为空或已过期，重新获取
+        if (accessToken == null || signId == null) {
+            JoySuchResponse<TokenEntity> response = getAccessToken(baseConfig.getJoysuch().getUsername(), baseConfig.getJoysuch().getPassword());
+            if (response == null || response.getData() == null) {
+                throw new RuntimeException("获取AccessToken失败");
+            }
+            accessToken = response.getData().getAccessToken();
+            signId = response.getData().getSignId();
+            long expireAt = response.getData().getExpireAt();
+
+            // 提前60秒过期，避免临界点失效
+            long ttl = expireAt - System.currentTimeMillis() - 60 * 1000;
+            if (ttl < 0) ttl = 60 * 1000; // 最少缓存1分钟
+
+            redisCache.setCacheObject(TOKEN_KEY, accessToken, (int)(ttl/1000), TimeUnit.SECONDS);
+            redisCache.setCacheObject(SIGNID_KEY, signId, (int)(ttl/1000), TimeUnit.SECONDS);
         }
-       if (response == null){
-           return new HashMap<>();
-       }
-//        Map<String, String> params = new HashMap<>();
-//        params.put("signId", response.getData().getSignId());
-//        params.put("accessToken", response.getData().getAccessToken());
-//        params.put("expireAt", String.valueOf(response.getData().getExpireAt()));
-//        params.put("x-token", getXToken(params.get("accessToken"), params.get("signId"), Long.parseLong(params.get("expireAt"))));
 
         Map<String, String> headers = new HashMap<>();
         headers.put("X-Token", getXToken(accessToken, signId, timestamp));
@@ -237,79 +191,55 @@ public class ZQOpenApi {
         headers.put("X-SignId", signId);
         headers.put("Accept-Charset", "utf-8");
         headers.put("contentType", "utf-8");
-        headers.put("X-BuildId", null);
         return headers;
     }
 
-    // 参考签名X-Token生成工具类
+    // X-Token生成
     public static String getXToken(String accessToken, String signId, Long timestamp){
-        return Md5Utils.md5(accessToken + Md5Util.md5(signId + Md5Util.md5(accessToken)).toLowerCase()+
+        return Md5Utils.md5(accessToken + Md5Util.md5(signId + Md5Util.md5(accessToken)).toLowerCase() +
                 timestamp).toLowerCase();
     }
 
-    public static String sendPost(String url, Map<String, String> headers, String jsonBody)
-    {
+    // 通用POST
+    public static String sendPost(String url, Map<String, String> headers, String jsonBody) {
         PrintWriter out = null;
         BufferedReader in = null;
         StringBuilder result = new StringBuilder();
-        try
-        {
+        try {
             log.info("sendPost - {}, body: {}", url, jsonBody);
             URL realUrl = new URL(url);
             URLConnection conn = realUrl.openConnection();
             conn.setDoOutput(true);
             conn.setDoInput(true);
             conn.setRequestProperty("Content-Type", "application/json;charset=utf-8");
-            // 添加自定义 header
             if (headers != null) {
                 for (Map.Entry<String, String> entry : headers.entrySet()) {
-                    conn.setRequestProperty(entry.getKey(), entry.getValue());
+                    if (entry.getValue() != null) {
+                        conn.setRequestProperty(entry.getKey(), entry.getValue());
+                    }
                 }
             }
-            // 写入 JSON 请求体
             out = new PrintWriter(conn.getOutputStream());
             out.print(jsonBody);
             out.flush();
-            // 读取响应
             in = new BufferedReader(new InputStreamReader(conn.getInputStream(), StandardCharsets.UTF_8));
             String line;
-            while ((line = in.readLine()) != null)
-            {
+            while ((line = in.readLine()) != null) {
                 result.append(line);
             }
-//            log.info("recv - {}", result);
-        }
-        catch (ConnectException e)
-        {
+        } catch (ConnectException e) {
             log.error("调用HttpUtils.sendPost ConnectException, url=" + url + ",param=" + jsonBody, e);
-        }
-        catch (SocketTimeoutException e)
-        {
+        } catch (SocketTimeoutException e) {
             log.error("调用HttpUtils.sendPost SocketTimeoutException, url=" + url + ",param=" + jsonBody, e);
-        }
-        catch (IOException e)
-        {
+        } catch (IOException e) {
             log.error("调用HttpUtils.sendPost IOException, url=" + url + ",param=" + jsonBody, e);
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             log.error("调用HttpsUtil.sendPost Exception, url=" + url + ",param=" + jsonBody, e);
-        }
-        finally
-        {
-            try
-            {
-                if (out != null)
-                {
-                    out.close();
-                }
-                if (in != null)
-                {
-                    in.close();
-                }
-            }
-            catch (IOException ex)
-            {
+        } finally {
+            try {
+                if (out != null) out.close();
+                if (in != null) in.close();
+            } catch (IOException ex) {
                 log.error("调用in.close Exception, url=" + url + ",param=" + jsonBody, ex);
             }
         }
@@ -324,29 +254,11 @@ public class ZQOpenApi {
 
     public JoySuchResponse<TokenEntity> getAccessToken(String username, String password) {
         AccessTokenReq accessTokenReq = new AccessTokenReq(getLicence(username, password));
-        JoySuchResponse<TokenEntity> response = AccessTokenApi.of(baseConfig.getJoysuch().getBaseUrl(), accessTokenReq.getLicence()).refreshAccessToken();
-        return response;
+        return AccessTokenApi.of(baseConfig.getJoysuch().getBaseUrl(), accessTokenReq.getLicence()).refreshAccessToken();
     }
 
     public JoySuchResponse<TokenEntity> refreshAccessToken() {
         AccessTokenReq accessTokenReq = new AccessTokenReq(licence);
-        JoySuchResponse<TokenEntity> response = AccessTokenApi.of(baseConfig.getJoysuch().getBaseUrl(), accessTokenReq.getLicence()).refreshAccessToken();
-        return response;
+        return AccessTokenApi.of(baseConfig.getJoysuch().getBaseUrl(), accessTokenReq.getLicence()).refreshAccessToken();
     }
-
-//    public static void main(String[] args) {
-////        getListOfCards();
-//        String startTime = "2025-08-12 15:00:00";
-//        String endTime = "2025-08-12 21:00:00";
-//        JSONObject jsonObject = JSONObject.parseObject(getListOfPoints("1918B3000BA3", "209885", startTime, endTime));
-//        // 指定要生成的 JSON 文件路径
-//        String filePath = "D:/PlatformData/定位卡数据/鱼嘴" + "/20250806下午zq.json";
-//
-//        try (FileWriter fileWriter = new FileWriter(filePath)) {
-//            fileWriter.write(jsonObject.toJSONString()); // fastjson 中使用 toJSONString() 方法
-//            System.out.println("JSON 文件已生成，路径为：" + filePath);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
 }
