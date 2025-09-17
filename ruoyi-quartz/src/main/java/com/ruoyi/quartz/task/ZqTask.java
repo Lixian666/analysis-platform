e1c8a9ca5a80a4067851cd6d361c6293439ed0c9
“Li” <735143929@qq.com>
2025-09-17 10:16:01 +0800

    feat(quartz): 添加 ZQGetTagStateHistoryOfTagID 方法- 新增 ZQGetTagStateHistoryOfTagID 方法，用于获取指定标签的历史状态信息 - 更新 buildingId 配置项，使用 joysuch.building-id - 添加相关依赖和工具类

--- ruoyi-quartz/src/main/java/com/ruoyi/quartz/task/ZqTask.java
+++ ruoyi-quartz/src/main/java/com/ruoyi/quartz/task/ZqTask.java
@@ -1,8 +1,12 @@
 package com.ruoyi.quartz.task;
 
+import com.alibaba.fastjson2.JSONArray;
+import com.alibaba.fastjson2.JSONObject;
+import com.jwzt.modules.experiment.utils.DateTimeUtils;
 import com.jwzt.modules.experiment.utils.third.zq.ZQOpenApi;
 import com.jwzt.modules.experiment.utils.third.zq.domain.SubReceiveData;
 import com.jwzt.modules.experiment.utils.third.zq.domain.SubscribeResult;
+import com.jwzt.modules.experiment.utils.third.zq.domain.TagScanUwbData;
 import com.ruoyi.quartz.domain.SysJobLog;
 import com.ruoyi.quartz.util.JobInvokeUtil;
 import org.slf4j.Logger;
@@ -11,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -32,8 +37,26 @@ public class ZqTask {
     private String domain;
     @Value("${server.port}")
     private String port;
-    @Value("${experiment.base.building-id}")
+    @Value("${experiment.base.joysuch.building-id}")
     private String buildingId;
+
+    public void ZQGetTagStateHistoryOfTagID()
+    {
+        String cardId = "1918B3000BA8";
+        String startTimeStr = "2025-09-12 14:17:00";
+        String endTimeStr = "2025-09-12 14:24:00";
+        JSONObject jsonObject = JSONObject.parseObject(zqOpenApi.getTagStateHistoryOfTagID(null, cardId, startTimeStr, endTimeStr));
+        JSONArray data = jsonObject.getJSONArray("data");
+        List<TagScanUwbData> tagScanUwbDataList = new ArrayList<>();
+        for (int i = 0; i < data.size(); i++) {
+            TagScanUwbData tag = data.getObject(i, TagScanUwbData.class);
+            tag.setDateTime(DateTimeUtils.timestampToDateTimeStr(tag.getTime()));
+            tagScanUwbDataList.add(tag);
+        }
+        JSONArray newData = new JSONArray(tagScanUwbDataList);
+        jsonObject.put("data", newData);
+        System.out.println(jsonObject);
+    }
 
     public void ZQHttpSubscriber(String type)
     {
