dbfb8f797381cc2d4fd972c8107abd82099a27c4
Miracle <735143929@qq.com>
2025-10-15 15:37:24 +0800

    feat(experiment): 添加信标信息数据导入功能接口
    - 在ITakBeaconInfoService接口中新增importTakBeaconInfo方法
    - 在TakBeaconInfoController中添加导入数据和下载模板接口
    - 实现TakBeaconInfoServiceImpl中的数据导入逻辑
    - 支持Excel文件导入信标信息
    - 支持选择是否更新已存在的数据
    - 添加导入结果的成功与失败信息反馈- 增加对空数据和异常情况的处理机制

--- modules/src/main/java/com/jwzt/modules/experiment/service/impl/TakBeaconInfoServiceImpl.java
+++ modules/src/main/java/com/jwzt/modules/experiment/service/impl/TakBeaconInfoServiceImpl.java
@@ -2,6 +2,7 @@ package com.jwzt.modules.experiment.service.impl;
 
 import java.util.List;
 import com.ruoyi.common.utils.DateUtils;
+import com.ruoyi.common.utils.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.jwzt.modules.experiment.mapper.TakBeaconInfoMapper;
@@ -92,5 +93,75 @@ public class TakBeaconInfoServiceImpl implements ITakBeaconInfoService
     public int deleteTakBeaconInfoById(String id)
     {
         return takBeaconInfoMapper.deleteTakBeaconInfoById(id);
+    }
+    
+    /**
+     * 导入信标信息数据
+     * 
+     * @param beaconInfoList 信标信息数据列表
+     * @param updateSupport 是否更新支持，如果已存在，是否更新
+     * @param operName 操作用户
+     * @return 结果
+     */
+    @Override
+    public String importTakBeaconInfo(List<TakBeaconInfo> beaconInfoList, boolean updateSupport, String operName)
+    {
+        if (StringUtils.isNull(beaconInfoList) || beaconInfoList.size() == 0)
+        {
+            throw new RuntimeException("导入信标信息数据不能为空！");
+        }
+        int successNum = 0;
+        int failureNum = 0;
+        StringBuilder successMsg = new StringBuilder();
+        StringBuilder failureMsg = new StringBuilder();
+        for (TakBeaconInfo beaconInfo : beaconInfoList)
+        {
+            try
+            {
+                // 验证是否存在这条记录
+                TakBeaconInfo existBeaconInfo = new TakBeaconInfo();
+                existBeaconInfo.setBeaconId(beaconInfo.getBeaconId());
+                List<TakBeaconInfo> existBeaconInfoList = takBeaconInfoMapper.selectTakBeaconInfoList(existBeaconInfo);
+                
+                if (StringUtils.isNull(existBeaconInfoList) || existBeaconInfoList.size() == 0)
+                {
+                    beaconInfo.setCreateBy(operName);
+                    beaconInfo.setCreateTime(DateUtils.getNowDate());
+                    this.insertTakBeaconInfo(beaconInfo);
+                    successNum++;
+                    successMsg.append("<br/>" + successNum + "、信标ID " + beaconInfo.getBeaconId() + " 导入成功");
+                }
+                else if (updateSupport)
+                {
+                    beaconInfo.setUpdateBy(operName);
+                    beaconInfo.setUpdateTime(DateUtils.getNowDate());
+                    beaconInfo.setId(existBeaconInfoList.get(0).getId());
+                    this.updateTakBeaconInfo(beaconInfo);
+                    successNum++;
+                    successMsg.append("<br/>" + successNum + "、信标ID " + beaconInfo.getBeaconId() + " 更新成功");
+                }
+                else
+                {
+                    failureNum++;
+                    failureMsg.append("<br/>" + failureNum + "、信标ID " + beaconInfo.getBeaconId() + " 已存在");
+                }
+            }
+            catch (Exception e)
+            {
+                failureNum++;
+                String msg = "<br/>" + failureNum + "、信标ID " + beaconInfo.getBeaconId() + " 导入失败：";
+                failureMsg.append(msg + e.getMessage());
+            }
+        }
+        if (failureNum > 0)
+        {
+            failureMsg.insert(0, "很抱歉，导入失败！共 " + failureNum + " 条数据格式不正确，错误如下：");
+            throw new RuntimeException(failureMsg.toString());
+        }
+        else
+        {
+            successMsg.insert(0, "恭喜您，数据已全部导入成功！共 " + successNum + " 条，数据如下：");
+        }
+        return successMsg.toString();
     }
 }
